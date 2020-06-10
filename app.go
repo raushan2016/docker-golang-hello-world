21e328d2fab1e5d2a535dafb8f82cea363841849
raushan2016 <raushankumar9666@gmail.com>
2020-06-10 14:57:04 -0700

    Adding writing to  file

--- app.go
+++ app.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -9,10 +10,44 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// WriteToFile will print any string of text to a file safely by
+// checking for errors and syncing at the end.
+func WriteToFile(filename string, data string) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = io.WriteString(file, data)
+	if err != nil {
+		return err
+	}
+	return file.Sync()
+}
+
+func createDirectory(directoryPath string) {
+	//choose your permissions well
+	pathErr := os.MkdirAll(directoryPath, 0777)
+
+	//check if you need to panic, fallback or report
+	if pathErr != nil {
+		fmt.Println(pathErr)
+		return
+	}
+
+	fmt.Println("Create the path", directoryPath)
+}
+
 func main() {
 
 	init := os.Getenv("INIT")
 	if init != "" {
+		createDirectory("/var/azureml-app")
+		err := WriteToFile("/var/azureml-app/result.txt", "Hello world, init done!!!!!!\n")
+		if err != nil {
+			fmt.Println(err)
+		}
 		fmt.Println("Hello world, init done!!!!!!")
 		return
 	}
